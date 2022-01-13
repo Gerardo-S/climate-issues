2792adf7c7b487ccb3f76417f058e8e383269b65
Gerardo Solis <gerardoa.solis@yahoo.com>
2022-01-12 19:48:20 -0800

    remove unused components

--- client/src/pages/IssuesPage.js
+++ client/src/pages/IssuesPage.js
@@ -3,7 +3,7 @@ import React from "react";
 function Issues() {
   return (
     <div className="homeContainer">
-      <h1>List of All Issues Page</h1>
+      <h1>Trending Post</h1>
     </div>
   );
 }
